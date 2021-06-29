0281d5a755ac032525cfb6b1a528de0aa8c2d9e7
Serhii Krimets <krimets.s88@gmail.com>
2021-06-29 03:05:56 +0300

    update timer

--- js/index.js
+++ js/index.js
@@ -9,6 +9,8 @@ class CountdownTimer {
             mins: document.querySelector('[data-value="mins"]'),
             secs: document.querySelector('[data-value="secs"]'),
         };
+        
+        this.dateTimer();
     }
 
     dateTimer() {
@@ -16,33 +18,25 @@ class CountdownTimer {
             let time = Date.parse(this.targetDate) - Date.parse(new Date());
 
             if (time > 0) {
-                const day = Math.floor(time / (1000 * 60 * 60 * 24));
-                const hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                const min = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-                const sec = Math.floor((time % (1000 * 60)) / 1000);
+                const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const days = Math.floor(time / (1000 * 60 * 60 * 24));
+                const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+                const secs = Math.floor((time % (1000 * 60)) / 1000);
 
-                this.updateTimer(this.refs.secs, sec);
-                this.updateTimer(this.refs.mins, min);
-                this.updateTimer(this.refs.hours, hour);
-                this.updateTimer(this.refs.days, day);
+                this.refs.days.textContent = days < 10 ? "0" + days : days;
+                this.refs.hours.textContent = hours < 10 ? "0" + hours : hours;
+                this.refs.mins.textContent = mins < 10 ? "0" + mins : mins;
+                this.refs.secs.textContent = secs < 10 ? "0" + secs : secs;
             } else {
                 clearInterval(timerInterval);;
             }
         }, 1000);
     }
-
-    updateTimer(nums, num) {
-        if (num <= 9) {
-        nums.textContent = '0' + num;
-        } else {
-        nums.textContent = num;
-        }
-    }
+    
 }
 
-const start = new CountdownTimer({
+const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 29, 2021'),
 });
 
-start.dateTimer();
