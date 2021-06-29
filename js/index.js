b793e77dfc20427c3416a5a1ce1563c7400b9222
Serhii Krimets <krimets.s88@gmail.com>
2021-06-29 03:28:39 +0300

    update timer

--- js/index.js
+++ js/index.js
@@ -13,26 +13,24 @@ class CountdownTimer {
         this.dateTimer();
     }
 
+    updateTime (value) {
+        return value < 10 ? "0" + value : value;
+    }
+
     dateTimer() {
         setInterval(() => {
             let time = Date.parse(this.targetDate) - Date.parse(new Date());
 
             if (time > 0) {
-                const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                const days = Math.floor(time / (1000 * 60 * 60 * 24));
-                const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-                const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-                this.refs.days.textContent = days < 10 ? "0" + days : days;
-                this.refs.hours.textContent = hours < 10 ? "0" + hours : hours;
-                this.refs.mins.textContent = mins < 10 ? "0" + mins : mins;
-                this.refs.secs.textContent = secs < 10 ? "0" + secs : secs;
+                this.refs.days.textContent = this.updateTime(Math.floor(time / (1000 * 60 * 60 * 24)));
+                this.refs.hours.textContent = this.updateTime(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+                this.refs.mins.textContent = this.updateTime(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+                this.refs.secs.textContent = this.updateTime(Math.floor((time % (1000 * 60)) / 1000));
             } else {
                 clearInterval(timerInterval);;
             }
         }, 1000);
     }
-    
 }
 
 const timer = new CountdownTimer({
